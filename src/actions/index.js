1381b62b1d5dbc5ad743b3dbcd572ceccc72977c
Taras Mazurkevych <mad.taras@gmail.com>
2017-05-20 18:02:13 +0300

    Create actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,11 @@
 import { createAction } from 'redux-act';
 
-export const textChanged = createAction('Text change');
+export const textChanged = createAction('Text changed');
+export const textSelected = createAction('Text selected');
+
+export const play = createAction('Start reading');
+export const pause = createAction('Pause reading');
+export const tick = createAction('Change the word in spritz-box');
+
+export const increaseSpeed = createAction('Increase reading speed');
+export const decreaseSpeed = createAction('Decrease speed');
